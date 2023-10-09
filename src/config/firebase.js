2fc8b675f063e8c98876ad02ada482ab88a539bb
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2023-10-09 15:40:17 +0100

    configured firebase project to use for backend

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBk9natbybSJFPINEPkpcC_gwOOJZKZkFM",
+  authDomain: "dungeon-crafters-notebook.firebaseapp.com",
+  projectId: "dungeon-crafters-notebook",
+  storageBucket: "dungeon-crafters-notebook.appspot.com",
+  messagingSenderId: "68474012183",
+  appId: "1:68474012183:web:fd0941ad84a6e7eabc1252",
+  measurementId: "G-9SN1Z38ZBM"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
