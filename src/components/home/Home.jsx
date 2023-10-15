936fbdbc905dc4234d126632b0db7728059d2fdb
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2023-10-15 23:07:41 +0100

    created home page tabs

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home({user,logout}){
+    const [tabSelected, setTabSelected] = useState("select_game")
+
     return(
     <div className='menu'>
-    {user
+    {!user
         ?
         <>
-            <h1>Home</h1>
-            <p>{user.email}</p>
+            <div className='tab-container'>
+                {tabSelected === "select_game"
+                ?<button className='tab selected'>Select Game</button>
+                :<button className='tab' onClick={() => setTabSelected("select_game")}>Select Game</button>
+                }
+                {tabSelected === "create_game"
+                ?<button className='tab selected'>Create Game</button>
+                :<button className='tab' onClick={() => setTabSelected("create_game")}>Create Game</button>
+                }
+                {tabSelected === "account"
+                ?<button className='tab selected'>Account</button>
+                :<button className='tab' onClick={() => setTabSelected("account")}>Account</button>
+                }
+                {tabSelected === "about"
+                ?<button className='tab selected'>About</button>
+                :<button className='tab' onClick={() => setTabSelected("about")}>About</button>
+                }
+            </div>
+            <div>
+                <h1>Home</h1>
+            </div>
         </>
 
         :
