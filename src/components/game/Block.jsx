d0eb4a6589f320dcabe2a97cef56a1619d7a4b47
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2023-11-24 14:48:20 +0000

    added save new block functionality

--- src/components/game/Block.jsx
+++ src/components/game/Block.jsx
@@ -1,9 +1,35 @@
-
+import { useState } from "react"
+import { db } from "../../config/firebase"
+import { addDoc, collection } from "firebase/firestore"
 
 export default function FolderList({block, getBlockData, close}){
+    const [processFinnished,setProcessFinnished] = useState(true)
 
-    async function Save(){
+    function Save(e){
+        e.preventDefault()
+        setProcessFinnished(false)
+        try{
+            // checks wether to save new block or to update existing one
+            if (block.new){ AddNewBlock(block) }
+            UpdateBlock(block)
+        }
+        catch(error){
+            console.error(error)
+            alert("Failed to save this block, try again later.")
+        }
+        finally{
+            getBlockData()// refreshes block data
+            setProcessFinnished(true)
+        }
+    }
 
+    async function AddNewBlock(block){
+        delete block.new// removes new property
+        await addDoc(collection(db, "Blocks"), block)// adds new block to firebase
+        
+    }
+    async function UpdateBlock(block){
+        
     }
 
     async function Delete(){
@@ -14,17 +40,30 @@ export default function FolderList({block, getBlockData, close}){
 
     return(
         <div className="block">
-            <button className="x-btn" onClick={close}>X</button>
-            <input
-                value={block.title}
-                className="block-title"/><br/>
-            <textarea
-                value={block.content}
-                className="block-content"/><br/>
-            <button className="form-btn" style={{"marginRight":"10px"}}>Save</button>
-            <button className="form-btn">Link</button>
-            <button className="form-btn" 
-                style={{"float":"right","backgroundColor":"red"}}>Delete</button>
+            <div>
+                <button className="x-btn" onClick={close}>X</button>
+
+                <input
+                    value={block.title}
+                    className="block-title"/><br/>
+                <textarea
+                    value={block.content}
+                    className="block-content"/><br/>
+            </div>
+
+            <div>
+                {processFinnished
+                ?<button className="form-btn" style={{"marginRight":"10px"}}
+                onClick={e => Save(e)}>Save</button>
+                :<button className="form-btn" style={{"marginRight":"10px"}}
+                    >Saving...</button>
+                }
+                
+                <button className="form-btn">Link</button>
+
+                {!block.new && <button className="form-btn" 
+                    style={{"float":"right","backgroundColor":"red"}}>Delete</button>}
+            </div>
         </div>
     )
 }
