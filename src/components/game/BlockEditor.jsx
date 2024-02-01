1f61282c77b6e999f284f770cd96ee6287bf2e23
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-02-01 21:33:45 +0000

    fixed issues when editting with tiny editor

--- src/components/game/BlockEditor.jsx
+++ src/components/game/BlockEditor.jsx
@@ -5,12 +5,14 @@ export default function BlockEditor({content,setContent}){
     return(
     <Editor
         apiKey="c70a4j85ev1e4q1dopyxbpw772r0lz047pef9umlig63xfdh"
-        initialValue={content}
+        value={content}
         onEditorChange={(e,editor) => setContent(editor.getContent())}
         init={{
             height: 360,
             menubar: false,
             browser_spellcheck: true,
+            newline_behavior: "linebreak",
+            resize:false,
             toolbar:
                 "undo redo | blocks | " +
                 "bold italic forecolor | alignleft aligncenter " +
