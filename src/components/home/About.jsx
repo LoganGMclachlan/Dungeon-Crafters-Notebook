1886c04e9c68d69feb9a10dab15e91265e82ca6a
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-02-26 19:38:51 +0000

    added survey link to about page

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,21 +1,19 @@
 export default function About(){
     return(
-        <div>
+        <div style={{"textAlign":"left"}}>
             <h1>About</h1>
-            <ul style={{"listStyle":"none","margin-left":"-40px"}}>
-                <li>Dungeon Crafters Notebook is a campeign manegement tool
-                    for game master of a TTRPG.</li>
-                <li>Here you can create games to store
-                    your lore and world info as well as link them together for easy
-                    traversal.</li>
-                <li>Create boards to group your info any way you want</li>
-                <li>Easily share your info with your players with a simple link.</li>
-            </ul>
-            <p>I am currently testing this website and would apprieciete any feedback. 
-                If you care to help, 
-                // TODO
-                <a url="">please fill out this quick questionare.</a>
+            <p>
+                Dungeon Crafters Notebook is a campaign manegement tool
+                for TTRPGs. It can create games to store your notes, lore, 
+                quests and more. You can even link together info or group 
+                them into boards to make finding what you need a breeze. 
+                Use templates for faster creation and share your work 
+                with a click.
             </p>
+            <p>This website is currently undergoing testing and your 
+                feedback would be very much appreciated.</p>
+            <p><a className="form-btn" href="https://forms.gle/j7iwAA15k1GK5RUw7"
+                target="_blank">Take the Survey</a></p>
         </div>
     )
 }
