b9fa31fdad6c56c3cee9dfbc8b0250d6a3aa4f2e
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-07-14 23:54:30 +0100

    updated 404 page

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -1,3 +1,15 @@
-export default NoPage = () => {
-    return <h1 className="heading">Error: Page Not Found!</h1>
+import { Link, useNavigate } from "react-router-dom"
+import "./home/home.css"
+
+export default function NoPage(){
+    const navigate = useNavigate()
+
+    return(
+    <div className="menu">
+        <h1 className="title">Page Not Found!</h1>
+        <p style={{"textAlign":"center"}}>
+            <Link onClick={() => navigate("/")}>Return to Home</Link>
+        </p>
+    </div>
+    )
 }
