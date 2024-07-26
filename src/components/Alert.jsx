f1e06634b6a1926a460dd71bc88bcded242d4ea6
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-07-26 17:21:19 +0100

    alert closes when "return" key pressed

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,8 +1,11 @@
 import "./styles.css"
 import ReactDOM from 'react-dom';
+import { useHotkeys } from "react-hotkeys-hook";
 
 export default function Alert({message,type}){
     const close = () => ReactDOM.unmountComponentAtNode(document.getElementById("alert-container"));
+
+    useHotkeys("return", close)
 
     return(
         <span id="alert" className={type}>
