b6edfe01252ed848e65eefb7ae01200fea43111b
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-12-03 14:40:31 +0000

    fixed vulnerabilities

--- dist/assets/NoPage-702f733c.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as e}from"./index-74bb6a80.js";function a(){const t=s();return e.jsxs("div",{className:"menu",children:[e.jsx("h1",{className:"title",children:"Page Not Found!"}),e.jsx("p",{style:{textAlign:"center"},children:e.jsx("u",{onClick:()=>t("/"),children:"Return to Home"})})]})}export{a as default};
---
+++ dist/assets/NoPage-c778a008.js
@@ -0,0 +1,1 @@
+import{u as s,j as e}from"./index-07a858f8.js";function a(){const t=s();return e.jsxs("div",{className:"menu",children:[e.jsx("h1",{className:"title",children:"Page Not Found!"}),e.jsx("p",{style:{textAlign:"center"},children:e.jsx("u",{onClick:()=>t("/"),children:"Return to Home"})})]})}export{a as default};
